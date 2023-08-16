d6d00791ffb02bc65ab08c7c30d5f9054f43f522
Ritesh <riteshgharti121232@gmail.com>
2023-08-16 21:09:35 +0530

    change ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,42 @@
-import React from 'react'
-import {BrowserRouter ,  Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home'
-import Topbar from './component/topbar/Topbar'
-import Register from './register/Register'
-import Login from './pages/login/Login'
-import Settings from './pages/settings/Settings'
-import Single from './pages/single/Single'
-import Write from './pages/write/Write'
+import React from 'react';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Topbar from './component/topbar/Topbar';
 import { useContext } from 'react';
 import { Context } from './context/Context';
-import "./app.css"
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import Settings from './pages/settings/Settings';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import "./app.css";
 
+function App() {
+  const { user } = useContext(Context);
 
-const App = () => {
-
-  const {user} = useContext(Context);
-
-  return (
+  const Layout = () => (
     <div className="app">
-
-  
-    <BrowserRouter>
-    <Topbar/>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-
-        <Route path="/register" element={user ? <Home/> : <Register />} />
-
-        <Route path="/login" element={user ? <Home/>  :
-        <Login />} />
-
-        <Route path="/write" element={user ? <Write/> :
-        <Register />} /> 
-
-        <Route path="/settings" element={user ? <Settings /> : <Register/> } />
-
-        <Route path="/post/:postId" element={<Single />} />
-
-
-      </Routes>
-    </BrowserRouter>
+      <Topbar />
+      <Outlet />
     </div>
-  )
+  );
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/register', element: user ? <Home /> : <Register /> },
+        { path: '/login', element: user ? <Home /> : <Login /> },
+        { path: '/write', element: user ? <Write /> : <Register /> },
+        { path: '/settings', element: user ? <Settings /> : <Register /> },
+        { path: '/post/:postId', element: <Single /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- frontend/src/component/header/Header.jsx
+++ frontend/src/component/header/Header.jsx
@@ -1,12 +1,21 @@
-import "./header.css"
-import headerImg from "../../images/write.jpg"
+import "./header.css";
+import headerImg from "../../images/35.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="headerImg">
-        <img className="headerImage" src={headerImg} alt="" />
+      <div className="left">
+        <div className="headerDsc">
+          <h1 className="headerTitle">
+            Discover endless inspiration on our blog. Let our words fuel your creativity and
+            curiosity.
+          </h1>
+          <button className="headerBtn">Write</button>
+        </div>
+      </div>
+      <div className="right">
+        <img className="headerImg" src={headerImg} alt="" />
       </div>
     </div>
-  )
+  );
 }
